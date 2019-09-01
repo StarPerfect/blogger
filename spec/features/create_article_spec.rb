ea23901b2b63a796d3f12c8656c4e6b870841e8e
Corina Allen <StarPerfect@gmail.com>
2019-09-01 13:37:33 -0600

    Add create spec and move fill_in test code from show spec to create spec

---
+++ spec/features/create_article_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Create New Article' do
+  describe 'user follows create a new article link' do
+    it 'can complete new article form and create a new resource' do
+      visit articles_path
+
+      click_link 'Create a New Article'
+
+      expect(current_path).to eq(new_article_path)
+
+      fill_in 'article[title]', with: 'Article Show Test Title'
+      fill_in 'article[body]', with: 'Show article body'
+
+      click_button 'Create'
+      
+      expect(page).to have_content('Article Show Test Title')
+      expect(page).to have_content('Show article body')
+    end
+  end
+end
