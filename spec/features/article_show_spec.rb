2652c524e674bd58f916d185c5396af8fdb805a2
Corina Allen <StarPerfect@gmail.com>
2019-09-12 20:27:30 -0600

    Add comment specs to article show page spec

--- spec/features/article_show_spec.rb
+++ spec/features/article_show_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'Article Show Page' do
   describe 'they link from the articles index' do
     it 'displays information for one article' do
       new_article = Article.create!(title: 'New Title', body: 'New Body')
+      comment_1 = article.comment.create(author_name: 'Me', body: 'Commenty comments')
+      comment_2 = article.comment.create(author_name: 'You', body: 'So much to say!')
 
       visit articles_path
 
@@ -11,6 +13,10 @@ RSpec.describe 'Article Show Page' do
 
       expect(page).to have_content(new_article.title)
       expect(page).to have_content(new_article.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
